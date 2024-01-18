9069d6a2228b418ada09b1296bfdd2f5e520ae50
Edsamn <eduardomsribas@gmail.comm>
2024-01-18 00:16:48 -0300

    commit

---
+++ ex-extra/scripts.js
@@ -0,0 +1,68 @@
+// Crie uma função para cada item que peça para pessoa usuária um número e imprima de acordo com os tópicos  :
+
+
+//     a-) O numero pedido
+//let numero = Number(prompt('Digite um número'))
+// function questaoA (numero) {
+//     console.log(numero)
+// }
+// questaoA(numero)
+
+
+//     b-) Os numeros pares maiores que 10
+// function questaoB() {
+//   let numero = Number(prompt("Digite um número"))
+//   for (let i = 11; i <= numero; i++) {
+//     if (i % 2 === 0) {
+//       console.log(i);
+//     }
+//   }  
+// }
+// questaoB();
+
+
+//     c-) Todos os números de zero ao número passado
+// function questaoC() {
+//   let numero = Number(prompt("Digite um número"))
+//   for (let i = 0; i <= numero; i++) {
+//     console.log(i)
+//   }
+// }
+// questaoC()
+
+
+//     d-) Insira 5 vezes em uma lista os 5 numeros passados
+// let lista = []
+// function questaoD() {
+//   for (let i = 0; i <= 4; i++) {
+//     let numero = Number(prompt("Digite um número"))
+//     lista.push(numero)
+//   }
+//   console.log(lista)
+// }
+// questaoD()
+
+
+//     e-) armazena na lista 5 números e retorne o número cujo o indice é 3
+// let lista = []
+// function questaoE() {
+//   for (let i = 0; i <= 4; i++) {
+//     let numero = Number(prompt("Digite um número"))
+//     lista.push(numero)
+//   }
+//   console.log(lista[3])
+// }
+// questaoE()
+
+
+//     f-) Armazene uma lista de 5 números e multiplique todos os números por 3
+// let lista = []
+// function questaoF() {
+//   for (let i = 0; i <= 4; i++) {
+//     let numero = Number(prompt("Digite um número"))
+//     lista.push(numero)
+//   }
+//   let listaVezes3 = lista.map((numero) => numero * 3)
+//   console.log(listaVezes3)
+// }
+// questaoF()
